bc9d63c638d902bf94e524b396170dc27a6266cb
sulyman <kamaldeensulyman@gmail.com>
2021-01-26 15:25:27 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -105,3 +105,10 @@ $(document).ready(function () {
         return false;
     });
 });
+
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({'overflow':'visible'});
+  })
